d2f4a7c5d5f0900e6426bd0799650e09c8fa8a76
kimjy8937 <kimjy3520@gmail.com>
2025-06-17 11:28:43 +0900

    feat: init-folder-structure

---
+++ src/main/java/com/nexus/sion/feature/project/command/application/dto/request/CreateRequest.java
@@ -0,0 +1,10 @@
+package com.nexus.sion.feature.project.command.application.dto.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CreateRequest {
+/* CoffeechatCreateRequest 등으로 클래스 이름 변경하여 사용 */
+}
---
+++ src/main/java/com/nexus/sion/feature/project/command/application/service/CommandService.java
@@ -0,0 +1,10 @@
+//package com.sixmycat.catchy.feature.feed.command.application.service;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class CommandService {
+//    /* CoffeechatCommandService 등으로 변경하여 사용*/
+//}
