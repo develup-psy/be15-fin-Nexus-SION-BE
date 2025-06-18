d90ff61dd14c811f173d5ef5077a1033fe313daf
kimjy8937 <kimjy3520@gmail.com>
2025-06-18 11:55:24 +0900

    feat: MemberListRequest DTO 생성

---
+++ src/main/java/com/nexus/sion/feature/member/query/dto/request/MemberListRequest.java
@@ -0,0 +1,16 @@
+package com.nexus.sion.feature.member.query.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberListRequest {
+    private int page = 0;
+    private int size = 10;
+
+    private String status;        // ex: "AVAILABLE", "IN_PROJECT", "UNAVAILABLE"
+    private String nameInitial;
+    private String sortBy;
+    private String sortDir;
+}
