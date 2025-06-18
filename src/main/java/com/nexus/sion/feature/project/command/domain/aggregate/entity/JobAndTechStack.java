04ac8f7833613e205c515f474ac1174ff30e0f54
ckaudgh <audgh3260@naver.com>
2025-06-18 14:43:15 +0900

    project entity 수정 #54

--- src/main/java/com/nexus/sion/feature/project/command/domain/aggregate/entity/JobAndTechStack.java
+++ src/main/java/com/nexus/sion/feature/project/command/domain/aggregate/entity/JobAndTechStack.java
@@ -1,38 +1,41 @@
 package com.nexus.sion.feature.project.command.domain.aggregate.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+
+import lombok.*;
 
 @Entity
 @Table(name = "job_and_tech_stack")
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class JobAndTechStack {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "job_and_tech_stack_id")
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "job_and_tech_stack_id")
+  private Long id;
 
-    @Column(name = "tech_stack_id", length = 30, nullable = false)
-    private String techStackId;
+  @Column(name = "tech_stack_id", length = 30, nullable = false)
+  private String techStackId;
 
-    /*@ManyToOne
-    @JoinColumn(name = "project_and_job_id", nullable = false)
-    private ProjectAndJob projectAndJob;*/
+  /*@ManyToOne
+  @JoinColumn(name = "project_and_job_id", nullable = false)
+  private ProjectAndJob projectAndJob;*/
 
-    @Column(name = "project_and_job_id", nullable = false)
-    private String projectAndJob;
+  @Column(name = "project_and_job_id", nullable = false)
+  private String projectAndJob;
 
-    @Column(name = "priority")
-    private Integer priority;
+  @Column(name = "priority")
+  private Integer priority;
 
-    @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
+  @Column(name = "created_at", nullable = false)
+  private LocalDateTime createdAt;
 
-    @Column(name = "updated_at", nullable = false)
-    private LocalDateTime updatedAt;
+  @Column(name = "updated_at", nullable = false)
+  private LocalDateTime updatedAt;
 }
