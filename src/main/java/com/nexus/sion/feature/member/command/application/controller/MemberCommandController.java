7634f92776713fce8505c4f9a2278e7f45a89ae7
sezeme <stellaa223@gmail.com>
2025-06-19 11:55:26 +0900

    feat : controller 생성

--- src/main/java/com/nexus/sion/feature/member/command/application/controller/MemberCommandController.java
+++ src/main/java/com/nexus/sion/feature/member/command/application/controller/MemberCommandController.java
@@ -1,21 +1,33 @@
 package com.nexus.sion.feature.member.command.application.controller;
 
 import com.nexus.sion.common.dto.ApiResponse;
+import com.nexus.sion.feature.member.command.application.dto.request.UserCreateRequest;
+import com.nexus.sion.feature.member.command.application.service.UserCommandService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/members")
+@RequestMapping("/api/v1/members")
 @Tag(name = "", description = "")
 public class MemberCommandController {
 
+    private final UserCommandService userCommandService;
 
-    @PostMapping()
-    public ResponseEntity<ApiResponse<Void>> register(@Re)
+    @Operation(summary = "회원 가입", description = "회원 가입 기능")
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<Void>> register(@RequestBody UserCreateRequest request) {
+        userCommandService.registerUser(request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ApiResponse.success(null));
+    }
 
 }
