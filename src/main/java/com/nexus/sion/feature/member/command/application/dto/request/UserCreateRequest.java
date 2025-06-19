cc96ad4c550acffa73248fd654c4ed36814e4e5a
sezeme <stellaa223@gmail.com>
2025-06-19 11:56:53 +0900

    feat : add DTO

---
+++ src/main/java/com/nexus/sion/feature/member/command/application/dto/request/UserCreateRequest.java
@@ -0,0 +1,13 @@
+package com.nexus.sion.feature.member.command.application.dto.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class UserCreateRequest {
+    private final String employeeIdentificationNumber;
+    private final String name;
+    private final String email;
+    private final String password;
+}
