88570cbc3eed5db611f2f1233f2dc451e8cb6ef4
kimjy8937 <kimjy3520@gmail.com>
2025-06-20 15:26:18 +0900

    feat: 상세조회 service 구현

--- src/main/java/com/nexus/sion/feature/member/query/service/MemberQueryService.java
+++ src/main/java/com/nexus/sion/feature/member/query/service/MemberQueryService.java
@@ -2,10 +2,13 @@ package com.nexus.sion.feature.member.query.service;
 
 import com.nexus.sion.common.dto.PageResponse;
 import com.nexus.sion.feature.member.query.dto.request.MemberListRequest;
+import com.nexus.sion.feature.member.query.dto.response.MemberDetailResponse;
 import com.nexus.sion.feature.member.query.dto.response.MemberListResponse;
 
 public interface MemberQueryService {
-    PageResponse<MemberListResponse> getAllMembers(MemberListRequest request);
+  PageResponse<MemberListResponse> getAllMembers(MemberListRequest request);
 
-    PageResponse<MemberListResponse> searchMembers(String keyword, int page, int size);
+  PageResponse<MemberListResponse> searchMembers(String keyword, int page, int size);
+
+  MemberDetailResponse getMemberDetail(String employeeId);
 }
