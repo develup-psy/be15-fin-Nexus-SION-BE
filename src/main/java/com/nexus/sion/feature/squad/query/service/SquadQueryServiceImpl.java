17b50e0ab2b367abfc39179f0fcfb2489c6a9da9
kihongkim <clevermind2@naver.com>
2025-06-20 18:04:48 +0900

    squad Service 구현체 코드 작성

---
+++ src/main/java/com/nexus/sion/feature/squad/query/service/SquadQueryServiceImpl.java
@@ -0,0 +1,31 @@
+package com.nexus.sion.feature.squad.query.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.nexus.sion.exception.BusinessException;
+import com.nexus.sion.exception.ErrorCode;
+import com.nexus.sion.feature.squad.query.dto.request.SquadListRequest;
+import com.nexus.sion.feature.squad.query.dto.response.SquadListResponse;
+import com.nexus.sion.feature.squad.query.repository.SquadQueryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SquadQueryServiceImpl implements SquadQueryService {
+
+    private final SquadQueryRepository squadQueryRepository;
+
+    @Override
+    public List<SquadListResponse> findSquads(SquadListRequest request) {
+        List<SquadListResponse> squads = squadQueryRepository.findSquads(request);
+
+        if (squads == null || squads.isEmpty()) {
+            throw new BusinessException(ErrorCode.SQUAD_NOT_FOUND);
+        }
+
+        return squads;
+    }
+}
