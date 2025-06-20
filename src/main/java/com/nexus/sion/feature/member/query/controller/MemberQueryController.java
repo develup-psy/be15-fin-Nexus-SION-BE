98a6e18f0bb0319dde0dbfcef11ee634493dc2e0
kimjy8937 <kimjy3520@gmail.com>
2025-06-20 15:26:38 +0900

    feat: 상세조회 controller 구현

--- src/main/java/com/nexus/sion/feature/member/query/controller/MemberQueryController.java
+++ src/main/java/com/nexus/sion/feature/member/query/controller/MemberQueryController.java
@@ -1,12 +1,12 @@
 package com.nexus.sion.feature.member.query.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import com.nexus.sion.common.dto.ApiResponse;
 import com.nexus.sion.common.dto.PageResponse;
 import com.nexus.sion.feature.member.query.dto.request.MemberListRequest;
+import com.nexus.sion.feature.member.query.dto.response.MemberDetailResponse;
 import com.nexus.sion.feature.member.query.dto.response.MemberListResponse;
 import com.nexus.sion.feature.member.query.service.MemberQueryService;
 
@@ -17,19 +17,27 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/members")
 public class MemberQueryController {
 
-    private final MemberQueryService memberQueryService;
+  private final MemberQueryService memberQueryService;
 
-    @GetMapping
-    public ResponseEntity<ApiResponse<PageResponse<MemberListResponse>>> getMembers(
-                    @ModelAttribute MemberListRequest request) {
-        PageResponse<MemberListResponse> pageResponse = memberQueryService.getAllMembers(request);
-        return ResponseEntity.ok(ApiResponse.success(pageResponse));
-    }
+  @GetMapping
+  public ResponseEntity<ApiResponse<PageResponse<MemberListResponse>>> getMembers(
+      @ModelAttribute MemberListRequest request) {
+    PageResponse<MemberListResponse> pageResponse = memberQueryService.getAllMembers(request);
+    return ResponseEntity.ok(ApiResponse.success(pageResponse));
+  }
 
-    @GetMapping("/search")
-    public ApiResponse<PageResponse<MemberListResponse>> searchDevelopers(
-                    @RequestParam String keyword, @RequestParam(defaultValue = "0") int page,
-                    @RequestParam(defaultValue = "10") int size) {
-        return ApiResponse.success(memberQueryService.searchMembers(keyword, page, size));
-    }
+  @GetMapping("/search")
+  public ApiResponse<PageResponse<MemberListResponse>> searchDevelopers(
+      @RequestParam String keyword,
+      @RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "10") int size) {
+    return ApiResponse.success(memberQueryService.searchMembers(keyword, page, size));
+  }
+
+  @GetMapping("/{employeeId}")
+  public ResponseEntity<ApiResponse<MemberDetailResponse>> getDeveloperDetail(
+      @PathVariable String employeeId) {
+    MemberDetailResponse response = memberQueryService.getMemberDetail(employeeId);
+    return ResponseEntity.ok(ApiResponse.success(response));
+  }
 }
