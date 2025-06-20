b501e86b4b1c6dfa911734cbff0106204dd0e107
sezeme <stellaa223@gmail.com>
2025-06-20 16:19:39 +0900

    feat: redis에 refreshToken 저장

--- src/main/java/com/nexus/sion/feature/auth/command/application/service/AuthServiceImpl.java
+++ src/main/java/com/nexus/sion/feature/auth/command/application/service/AuthServiceImpl.java
@@ -1,7 +1,9 @@
 package com.nexus.sion.feature.auth.command.application.service;
 
+import com.nexus.sion.feature.auth.command.domain.aggregate.RefreshToken;
 import com.nexus.sion.feature.member.command.domain.aggregate.enums.MemberRole;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,12 +17,15 @@ import com.nexus.sion.security.jwt.JwtTokenProvider;
 
 import lombok.RequiredArgsConstructor;
 
+import java.time.Duration;
+
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
     private final JwtTokenProvider jwtTokenProvider;
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final RedisTemplate<String, RefreshToken> redisTemplate;
 
     /* 테스트 로그인 */
     @Transactional
@@ -38,14 +43,25 @@ public class AuthServiceImpl implements AuthService {
         Member member = memberRepository.findById(loginRequest.getEmployeeIdentificationNumber())
                 .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
 
-        if(!passwordEncoder.matches(loginRequest.getPassword(), member.getPassword())) {
+        if (!passwordEncoder.matches(loginRequest.getPassword(), member.getPassword())) {
             throw new BusinessException(ErrorCode.INVALID_CURRENT_PASSWORD);
         }
 
+        // 로그인 성공 시 token 발급
         String accessToken = jwtTokenProvider.createToken(member.getEmployeeIdentificationNumber(), member.getRole().name());
         String refreshToken = jwtTokenProvider.createRefreshToken(member.getEmployeeIdentificationNumber(), member.getRole().name());
 
-        // todo : redis에 저장해서 관리
+        // Redis에 value로 저장할 객체 생성
+        RefreshToken redisRefreshToken = RefreshToken.builder()
+                .token(refreshToken)
+                .build();
+
+        // Redis에 저장 (TTL: 7일)
+        redisTemplate.opsForValue().set(
+                member.getEmployeeIdentificationNumber(),
+                redisRefreshToken,
+                Duration.ofDays(7)
+        );
 
         return TokenResponse.builder()
                 .accessToken(accessToken)
