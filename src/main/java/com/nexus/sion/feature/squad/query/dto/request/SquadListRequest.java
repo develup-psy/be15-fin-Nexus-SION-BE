7bf9a93ef32d5fcf16992c431085708fbc67b6ec
kihongkim <clevermind2@naver.com>
2025-06-20 18:03:52 +0900

    squad DTO request 코드 작성

---
+++ src/main/java/com/nexus/sion/feature/squad/query/dto/request/SquadListRequest.java
@@ -0,0 +1,12 @@
+package com.nexus.sion.feature.squad.query.dto.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SquadListRequest {
+    private String projectCode;
+}
