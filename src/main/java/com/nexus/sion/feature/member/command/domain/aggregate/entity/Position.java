8bc0fe1798c21fd50cd2b34ed0da6fdc87e658c6
kimjy8937 <kimjy3520@gmail.com>
2025-06-23 20:27:35 +0900

    feat: Position, InitialScore, Department Entity 생성

---
+++ src/main/java/com/nexus/sion/feature/member/command/domain/aggregate/entity/Position.java
@@ -0,0 +1,37 @@
+package com.nexus.sion.feature.member.command.domain.aggregate.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "position")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Position {
+
+    @Id
+    @Column(name = "position_name", length = 100)
+    private String positionName;
+
+    @Column(name = "create_at", nullable = false, updatable = false)
+    private LocalDateTime createAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
