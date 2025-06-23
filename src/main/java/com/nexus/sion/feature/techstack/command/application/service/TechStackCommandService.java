d87b409b5e5057ebc0d64e6d748ab4f630aff3d0
sezeme <stellaa223@gmail.com>
2025-06-23 17:38:00 +0900

    feat: add service

---
+++ src/main/java/com/nexus/sion/feature/techstack/command/application/service/TechStackCommandService.java
@@ -0,0 +1,26 @@
+package com.nexus.sion.feature.techstack.command.application.service;
+
+import com.nexus.sion.feature.techstack.command.application.dto.request.TechStackCreateRequest;
+import com.nexus.sion.feature.techstack.command.domain.aggregate.TechStack;
+import com.nexus.sion.feature.techstack.command.repository.TechStackRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TechStackCommandService {
+
+    private final ModelMapper modelMapper;
+    private final TechStackRepository techStackRepository;
+
+    public void registerTechStack(TechStackCreateRequest request) {;
+        // 기존에 존재하는 기술스택은 저장하지 않고 종료
+        if(techStackRepository.existsById(request.getTechStackName())) {
+            return;
+        }
+
+        TechStack techStack = modelMapper.map(request, TechStack.class);
+        techStackRepository.save(techStack);
+    }
+}
