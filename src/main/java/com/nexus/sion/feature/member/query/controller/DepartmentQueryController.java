efdc1f0dbb8a9fe6de77c6e30e93c1aa3baf4f42
kimjy8937 <kimjy3520@gmail.com>
2025-06-24 15:40:49 +0900

    feat: DepartmetQueryController 구현

---
+++ src/main/java/com/nexus/sion/feature/member/query/controller/DepartmentQueryController.java
@@ -0,0 +1,26 @@
+package com.nexus.sion.feature.member.query.controller;
+
+import com.nexus.sion.common.dto.ApiResponse;
+import com.nexus.sion.feature.member.query.dto.response.DepartmentResponse;
+import com.nexus.sion.feature.member.query.service.DepartmentQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/departments")
+public class DepartmentQueryController {
+
+    private final DepartmentQueryService departmentQueryService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<DepartmentResponse>>> getAllDepartments() {
+        List<DepartmentResponse> departments = departmentQueryService.getDepartments();
+        return ResponseEntity.ok(ApiResponse.success(departments));
+    }
+}
