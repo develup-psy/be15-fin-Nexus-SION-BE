25cea39b467da396ad5f7703e48cc58a5ea91f9b
sezeme <stellaa223@gmail.com>
2025-06-24 16:03:11 +0900

    feat : add controller

--- src/main/java/com/nexus/sion/feature/project/command/application/controller/DomainCommandController.java
+++ src/main/java/com/nexus/sion/feature/project/command/application/controller/DomainCommandController.java
@@ -2,8 +2,10 @@ package com.nexus.sion.feature.project.command.application.controller;
 
 import com.nexus.sion.common.dto.ApiResponse;
 import com.nexus.sion.feature.project.command.application.dto.request.DomainRequest;
+import com.nexus.sion.feature.project.command.application.service.DomainCommandService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/domains")
 public class DomainCommandController {
 
+    private final DomainCommandService domainCommandService;
+
     @PostMapping
     @Operation(summary = "도메인 등록", description = "새로운 도메인을 시스템에 등록합니다.")
     public ResponseEntity<ApiResponse<Void>> registerDomain(
             @RequestBody DomainRequest request) {
-
+        domainCommandService.registerDomain(request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ApiResponse.success(null));
     }
 }
