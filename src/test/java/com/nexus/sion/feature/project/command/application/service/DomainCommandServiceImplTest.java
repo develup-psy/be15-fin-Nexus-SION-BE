2cc07e1eb43c0a63749e86f23a107bc1feb4b124
sezeme <stellaa223@gmail.com>
2025-06-24 16:35:28 +0900

    test: add service test

---
+++ src/test/java/com/nexus/sion/feature/project/command/application/service/DomainCommandServiceImplTest.java
@@ -0,0 +1,52 @@
+package com.nexus.sion.feature.project.command.application.service;
+
+import com.nexus.sion.feature.project.command.application.dto.request.DomainRequest;
+import com.nexus.sion.feature.project.command.domain.aggregate.Domain;
+import com.nexus.sion.feature.project.command.repository.DomainRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class DomainCommandServiceImplTest {
+    @InjectMocks private DomainCommandServiceImpl domainCommandService;
+
+    @Mock private DomainRepository domainRepository;
+
+    @Mock private ModelMapper modelMapper;
+
+    String domainName = "제조";
+
+    @Test
+    void registerDomain_이미존재하면저장하지않음() {
+        // given
+        DomainRequest request = new DomainRequest(domainName);
+        when(domainRepository.existsById(domainName)).thenReturn(true);
+
+        // when
+        domainCommandService.registerDomain(request);
+
+        // then
+        verify(domainRepository, never()).save(any(Domain.class));
+    }
+
+    @Test
+    void registerDomain_존재하지않으면저장() {
+        // given
+        DomainRequest request = new DomainRequest(domainName);
+        when(domainRepository.existsById(domainName)).thenReturn(false);
+        when(modelMapper.map(request, Domain.class)).thenReturn(mock(Domain.class));
+
+        // when
+        domainCommandService.registerDomain(request);
+
+        // then
+        verify(domainRepository, times(1)).save(any(Domain.class));
+    }
+}
