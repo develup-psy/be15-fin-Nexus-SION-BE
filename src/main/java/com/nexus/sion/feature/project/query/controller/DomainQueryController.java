ccff3aaf648858d44b3510cdb761b6066a0facc3
sezeme <stellaa223@gmail.com>
2025-06-25 09:44:24 +0900

    feat : add controller

---
+++ src/main/java/com/nexus/sion/feature/project/query/controller/DomainQueryController.java
@@ -0,0 +1,26 @@
+package com.nexus.sion.feature.project.query.controller;
+
+import com.nexus.sion.common.dto.ApiResponse;
+import com.nexus.sion.feature.project.query.dto.response.DomainListResponse;
+import com.nexus.sion.feature.project.query.service.DomainQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/domains")
+public class DomainQueryController {
+
+    private final DomainQueryService domainQueryService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<DomainListResponse>> getAllDomains() {
+        return ResponseEntity.ok(
+                ApiResponse.success(new DomainListResponse(domainQueryService.findAllDomains())));
+        )
+
+    }
+}
