c787c4c1bc6b1fb25aa0b30b6f176a9d7657623d
sezeme <stellaa223@gmail.com>
2025-06-25 15:47:19 +0900

    feat : add repository

---
+++ src/main/java/com/nexus/sion/feature/project/query/repository/JobQueryRepository.java
@@ -0,0 +1,23 @@
+package com.nexus.sion.feature.project.query.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.example.jooq.generated.tables.Job.JOB;
+
+@Repository
+@RequiredArgsConstructor
+public class JobQueryRepository {
+    private final DSLContext dsl;
+
+    public List<String> findAllJobs() {
+        return dsl
+                .select(JOB.NAME)
+                .from(JOB)
+                .orderBy(JOB.NAME.asc())
+                .fetchInto(String.class);
+    }
+}
