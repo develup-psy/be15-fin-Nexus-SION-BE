86cef302fb11cc25c2ad1f97b36517e0889ada7b
kimjy8937 <kimjy3520@gmail.com>
2025-06-25 09:28:58 +0900

    test: 관련 테스트 케이스 수정

--- src/test/java/com/nexus/sion/feature/member/query/service/DepartmentQueryServiceTest.java
+++ src/test/java/com/nexus/sion/feature/member/query/service/DepartmentQueryServiceTest.java
@@ -6,6 +6,7 @@ import static org.mockito.MockitoAnnotations.openMocks;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
