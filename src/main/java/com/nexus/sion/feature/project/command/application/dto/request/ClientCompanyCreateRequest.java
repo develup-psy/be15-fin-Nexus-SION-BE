2517673985db91997b7a269af1ce3468bb74b912
sezeme <stellaa223@gmail.com>
2025-06-25 17:25:02 +0900

    feat: add client company create request

--- src/main/java/com/nexus/sion/feature/project/command/application/dto/request/ClientCompanyCreateRequest.java
+++ src/main/java/com/nexus/sion/feature/project/command/application/dto/request/ClientCompanyCreateRequest.java
@@ -1,28 +1,19 @@
 package com.nexus.sion.feature.project.command.application.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-/*
-*
-CREATE TABLE
-        `client_company` (
-                `client_code` VARCHAR(30) NOT NULL,
-                `company_name` VARCHAR(255) NULL,
-                `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                `contact_person` VARCHAR(10) NULL,
-                `email` VARCHAR(30) NULL,
-                `contact_number` VARCHAR(21) NULL,
-                `domain_name` VARCHAR(30) NOT NULL,
-                PRIMARY KEY (`client_code`)
-        );
-* */
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class ClientCompanyCreateRequest {
-
+    @NotBlank String companyName;
+    @NotBlank String domainName;
+    String contactPerson;
+    String email;
+    String contactNumber;
 }
