a89cdf0a23e51ee2bd34312dd300fc4cbd11e26e
sezeme <stellaa223@gmail.com>
2025-06-25 10:15:59 +0900

    feat : add request dto

---
+++ src/main/java/com/nexus/sion/feature/project/command/application/dto/request/JobRequest.java
@@ -0,0 +1,12 @@
+package com.nexus.sion.feature.project.command.application.dto.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JobRequest {
+    String jobName;
+}
