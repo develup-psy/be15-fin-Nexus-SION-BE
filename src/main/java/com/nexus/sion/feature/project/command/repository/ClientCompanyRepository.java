e2d640c45d6736aab00268e5d12b3ecca2e9fc6b
sezeme <stellaa223@gmail.com>
2025-06-26 11:24:05 +0900

    feat: add reopsitory

--- src/main/java/com/nexus/sion/feature/project/command/repository/ClientCompanyRepository.java
+++ src/main/java/com/nexus/sion/feature/project/command/repository/ClientCompanyRepository.java
@@ -4,6 +4,9 @@ import com.nexus.sion.feature.project.command.domain.aggregate.ClientCompany;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClientCompanyRepository extends JpaRepository<ClientCompany, String> {
+        Optional<ClientCompany> findTopByClientCodeStartingWithOrderByClientCodeDesc(String codePrefix);
 }
