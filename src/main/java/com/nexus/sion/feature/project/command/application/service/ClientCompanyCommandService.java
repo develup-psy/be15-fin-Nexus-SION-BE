8e9749a37aaee94847ac21bea845fc954ca4e26b
sezeme <stellaa223@gmail.com>
2025-06-26 16:56:15 +0900

    feat : add service

--- src/main/java/com/nexus/sion/feature/project/command/application/service/ClientCompanyCommandService.java
+++ src/main/java/com/nexus/sion/feature/project/command/application/service/ClientCompanyCommandService.java
@@ -1,7 +1,11 @@
 package com.nexus.sion.feature.project.command.application.service;
 
 import com.nexus.sion.feature.project.command.application.dto.request.ClientCompanyCreateRequest;
+import com.nexus.sion.feature.project.command.application.dto.request.ClientCompanyUpdateRequest;
+import jakarta.validation.Valid;
 
 public interface ClientCompanyCommandService {
   void registerClientCompany(ClientCompanyCreateRequest request);
+
+  void updateClientCompany(@Valid ClientCompanyUpdateRequest request, String clientCode);
 }
