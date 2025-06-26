6b14fe9a2d6482b7eed808beb248baebb8eb92ac
kihongkim <clevermind2@naver.com>
2025-06-26 15:07:30 +0900

    deleteBySquadCode 메서드 추가, * 스쿼드 수정 시 기존 구성원을 모두 삭제하기 위함

--- src/main/java/com/nexus/sion/feature/squad/command/repository/SquadEmployeeCommandRepository.java
+++ src/main/java/com/nexus/sion/feature/squad/command/repository/SquadEmployeeCommandRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nexus.sion.feature.squad.command.domain.aggregate.entity.SquadEmployee;
 
-public interface SquadEmployeeCommandRepository extends JpaRepository<SquadEmployee, Long> {}
+public interface SquadEmployeeCommandRepository extends JpaRepository<SquadEmployee, Long> {
+    void deleteBySquadCode(String squadCode);
+}
