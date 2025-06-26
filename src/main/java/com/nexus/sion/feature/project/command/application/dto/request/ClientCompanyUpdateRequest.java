54ab91a01bdc57ac89d9c3588395e3886a85691c
sezeme <stellaa223@gmail.com>
2025-06-26 16:56:39 +0900

    feat : 유효성 검사 로직 추가

--- src/main/java/com/nexus/sion/feature/project/command/application/dto/request/ClientCompanyUpdateRequest.java
+++ src/main/java/com/nexus/sion/feature/project/command/application/dto/request/ClientCompanyUpdateRequest.java
@@ -1,5 +1,7 @@
 package com.nexus.sion.feature.project.command.application.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Getter
@@ -11,6 +13,11 @@ public class ClientCompanyUpdateRequest {
     String companyName;
     String domainName;
     String contactPerson;
+    @Email(message = "유효한 이메일 형식이 아닙니다.")
     String email;
+    @Pattern(
+            regexp = "^01[016789]-\\d{3,4}-\\d{4}$",
+            message = "유효한 휴대전화 번호 형식이 아닙니다. 예: 010-1234-5678"
+    )
     String contactNumber;
 }
