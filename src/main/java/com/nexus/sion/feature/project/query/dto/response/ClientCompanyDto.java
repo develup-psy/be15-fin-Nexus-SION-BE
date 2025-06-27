539cf292c9a60c5c6e5149dad509c763fbb1adef
sezeme <stellaa223@gmail.com>
2025-06-27 10:47:00 +0900

    feat: add response dto

---
+++ src/main/java/com/nexus/sion/feature/project/query/dto/response/ClientCompanyDto.java
@@ -0,0 +1,10 @@
+package com.nexus.sion.feature.project.query.dto.response;
+
+public class ClientCompanyDto {
+    String clientCode;
+    String companyName;
+    String domainName;
+    String contactPerson;
+    String email;
+    String contactNumber;
+}
