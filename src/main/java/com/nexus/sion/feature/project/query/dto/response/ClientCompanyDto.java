7c0fc8a989b8fa346aab149936419916797d5433
sezeme <stellaa223@gmail.com>
2025-06-27 11:55:46 +0900

    fix : jackson 직렬화 가능하도록 수정

--- src/main/java/com/nexus/sion/feature/project/query/dto/response/ClientCompanyDto.java
+++ src/main/java/com/nexus/sion/feature/project/query/dto/response/ClientCompanyDto.java
@@ -1,5 +1,12 @@
 package com.nexus.sion.feature.project.query.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data  // 또는 @Getter + @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClientCompanyDto {
     String clientCode;
     String companyName;
