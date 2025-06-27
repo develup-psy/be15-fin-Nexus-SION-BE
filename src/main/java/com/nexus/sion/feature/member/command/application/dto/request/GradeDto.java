2c2afef0b7127b12feb590af8432f8e127ddc85f
sezeme <stellaa223@gmail.com>
2025-06-27 20:13:15 +0900

    feat: add annotation

--- src/main/java/com/nexus/sion/feature/member/command/application/dto/request/GradeDto.java
+++ src/main/java/com/nexus/sion/feature/member/command/application/dto/request/GradeDto.java
@@ -1,11 +1,17 @@
 package com.nexus.sion.feature.member.command.application.dto.request;
 
 import com.nexus.sion.feature.member.command.domain.aggregate.enums.GradeCode;
+import lombok.*;
 
 import java.math.BigDecimal;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Setter
 public class GradeDto {
     private GradeCode gradeCode;
     private BigDecimal productivity;
-    private int monthlyUnitPrice;
+    private Integer monthlyUnitPrice;
 }
