e342a1860624fb93706e402287dc0fc93be752e8
sezeme <stellaa223@gmail.com>
2025-06-27 20:12:28 +0900

    fix: service 로직 수정

---
+++ src/main/java/com/nexus/sion/feature/member/command/application/service/GradeCommandService.java
@@ -0,0 +1,7 @@
+package com.nexus.sion.feature.member.command.application.service;
+
+import com.nexus.sion.feature.member.command.application.dto.request.UnitPriceSetRequest;
+
+public interface GradeCommandService {
+    void setGrades(UnitPriceSetRequest request);
+}
