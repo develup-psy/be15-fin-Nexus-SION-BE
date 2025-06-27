2f09342eada120dbbb2d9d2a253913272c93ca21
kihongkim <clevermind2@naver.com>
2025-06-27 20:15:26 +0900

    스쿼드 페이징 관련 repository 코드 추가

--- src/main/java/com/nexus/sion/feature/squad/query/repository/SquadQueryRepository.java
+++ src/main/java/com/nexus/sion/feature/squad/query/repository/SquadQueryRepository.java
@@ -11,8 +11,11 @@ import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
+import com.example.jooq.generated.tables.records.SquadRecord;
+import com.nexus.sion.feature.squad.query.dto.response.SquadListResultResponse;
 import org.jooq.*;
 import org.jooq.Record;
 import org.springframework.stereotype.Repository;
@@ -32,8 +35,10 @@ public class SquadQueryRepository {
 
   private final DSLContext dsl;
 
-  public List<SquadListResponse> findSquads(SquadListRequest request) {
+  public SquadListResultResponse findSquads(SquadListRequest request) {
     String projectCode = request.getProjectCode();
+      int page = request.getPage();
+      int size = request.getSize();
 
     Map<String, List<SquadListResponse.MemberInfo>> memberMap =
         dsl.select(SQUAD_EMPLOYEE.SQUAD_CODE, MEMBER.EMPLOYEE_NAME, PROJECT_AND_JOB.JOB_NAME)
@@ -50,36 +55,49 @@ public class SquadQueryRepository {
                     new SquadListResponse.MemberInfo(
                         r.get(MEMBER.EMPLOYEE_NAME), r.get(PROJECT_AND_JOB.JOB_NAME)));
 
-    return dsl
-        .selectFrom(SQUAD)
-        .where(SQUAD.PROJECT_CODE.eq(projectCode))
-        .orderBy(SQUAD.CREATED_AT.desc())
-        .fetch()
-        .stream()
-        .map(
-            r -> {
-              String code = r.get(SQUAD.SQUAD_CODE);
-              String name = r.get(SQUAD.TITLE);
+      Result<SquadRecord> records = dsl
+              .selectFrom(SQUAD)
+              .where(SQUAD.PROJECT_CODE.eq(projectCode))
+              .orderBy(SQUAD.CREATED_AT.desc())
+              .limit(size)
+              .offset(page * size)
+              .fetch();
 
-              SquadOriginType originType = r.get(SQUAD.ORIGIN_TYPE);
-              boolean isAiRecommended = SquadOriginType.AI.equals(originType);
+      Long total = Objects.requireNonNullElse(
+              dsl.selectCount()
+                      .from(SQUAD)
+                      .where(SQUAD.PROJECT_CODE.eq(projectCode))
+                      .fetchOne(0, Long.class),
+              0L
+      );
 
-              LocalDate start = r.get(SQUAD.CREATED_AT).toLocalDate();
-              LocalDate end = start.plusMonths(r.get(SQUAD.ESTIMATED_DURATION).longValue());
-              String period = start + " ~ " + end;
+      List<SquadListResponse> content = records
+              .stream()
+              .map(r -> {
+                  String code = r.get(SQUAD.SQUAD_CODE);
+                  String name = r.get(SQUAD.TITLE);
 
-              DecimalFormat decimalFormat = new DecimalFormat("#,###");
-              String cost = "₩" + decimalFormat.format(r.get(SQUAD.ESTIMATED_COST));
+                  SquadOriginType originType = r.get(SQUAD.ORIGIN_TYPE);
+                  boolean isAiRecommended = SquadOriginType.AI.equals(originType);
 
-              return new SquadListResponse(
-                  code,
-                  name,
-                  isAiRecommended,
-                  memberMap.getOrDefault(code, List.of()),
-                  period,
-                  cost);
-            })
-        .toList();
+                  LocalDate start = r.get(SQUAD.CREATED_AT).toLocalDate();
+                  LocalDate end = start.plusMonths(r.get(SQUAD.ESTIMATED_DURATION).longValue());
+                  String period = start + " ~ " + end;
+
+                  DecimalFormat decimalFormat = new DecimalFormat("#,###");
+                  String cost = "₩" + decimalFormat.format(r.get(SQUAD.ESTIMATED_COST));
+
+                  return new SquadListResponse(
+                          code,
+                          name,
+                          isAiRecommended,
+                          memberMap.getOrDefault(code, List.of()),
+                          period,
+                          cost);
+              })
+              .toList();
+
+      return new SquadListResultResponse(content, page, size, total);
   }
 
   public SquadDetailResponse findSquadDetailByCode(String squadCode) {
