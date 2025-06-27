b34fd01be50e25910f344171581095d4807b6746
sezeme <stellaa223@gmail.com>
2025-06-27 15:39:55 +0900

    feat: repository 검색 조건 추가할 수 있도록 변경

--- src/main/java/com/nexus/sion/feature/project/query/repository/ClientCompanyQueryRepository.java
+++ src/main/java/com/nexus/sion/feature/project/query/repository/ClientCompanyQueryRepository.java
@@ -5,7 +5,9 @@ import static com.nexus.sion.common.constants.CollationConstants.UTF8MB4_UNICODE
 
 import java.util.List;
 
+import org.jooq.Condition;
 import org.jooq.DSLContext;
+import org.jooq.SortField;
 import org.springframework.stereotype.Repository;
 
 import com.nexus.sion.feature.project.query.dto.response.ClientCompanyDto;
@@ -17,16 +19,29 @@ import lombok.RequiredArgsConstructor;
 public class ClientCompanyQueryRepository {
   private final DSLContext dsl;
 
-  public List<ClientCompanyDto> findAllClientCompanies() {
+
+  public List<ClientCompanyDto> findAllByCondition(Condition condition, SortField<?> sortField, int page, int size) {
     return dsl.select(
-            CLIENT_COMPANY.CLIENT_CODE,
-            CLIENT_COMPANY.COMPANY_NAME,
-            CLIENT_COMPANY.DOMAIN_NAME,
-            CLIENT_COMPANY.CONTACT_PERSON,
-            CLIENT_COMPANY.EMAIL,
-            CLIENT_COMPANY.CONTACT_NUMBER)
-        .from(CLIENT_COMPANY)
-        .orderBy(CLIENT_COMPANY.COMPANY_NAME.collate(UTF8MB4_UNICODE_520_CI).asc())
-        .fetchInto(ClientCompanyDto.class);
+                    CLIENT_COMPANY.CLIENT_CODE,
+                    CLIENT_COMPANY.COMPANY_NAME,
+                    CLIENT_COMPANY.DOMAIN_NAME,
+                    CLIENT_COMPANY.CONTACT_PERSON,
+                    CLIENT_COMPANY.EMAIL,
+                    CLIENT_COMPANY.CONTACT_NUMBER
+            )
+            .from(CLIENT_COMPANY)
+            .where(condition)
+            .orderBy(sortField)
+            .offset(page * size)
+            .limit(size)
+            .fetchInto(ClientCompanyDto.class);
+  }
+
+  public long countByCondition(Condition condition) {
+    Long count = dsl.selectCount()
+            .from(CLIENT_COMPANY)
+            .where(condition)
+            .fetchOne(0, long.class); // 첫 번째 열을 Long으로 가져옴
+    return count != null ? count : 0L;
   }
 }
