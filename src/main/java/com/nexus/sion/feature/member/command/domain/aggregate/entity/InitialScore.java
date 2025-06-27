df92756a07c48cd4b3bfba18ce8e5a0a9f73e9d6
sezeme <stellaa223@gmail.com>
2025-06-27 21:46:17 +0900

    refactor : 엔터티 수정

--- src/main/java/com/nexus/sion/feature/member/command/domain/aggregate/entity/InitialScore.java
+++ src/main/java/com/nexus/sion/feature/member/command/domain/aggregate/entity/InitialScore.java
@@ -1,7 +1,6 @@
 package com.nexus.sion.feature.member.command.domain.aggregate.entity;
 
-import java.time.LocalDateTime;
-
+import com.nexus.sion.common.domain.BaseTimeEntity;
 import jakarta.persistence.*;
 
 import lombok.*;
@@ -12,32 +11,18 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-public class InitialScore {
+public class InitialScore extends BaseTimeEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(nullable = false)
-  private int years;
+  @Column(name = "min_years", nullable = false)
+  private int minYears;
+
+  @Column(name = "max_years")
+  private int maxYears;
 
   @Column(nullable = false)
   private int score;
-
-  @Column(name = "created_at", nullable = false, updatable = false)
-  private LocalDateTime createdAt;
-
-  @Column(name = "updated_at", nullable = false)
-  private LocalDateTime updatedAt;
-
-  @PrePersist
-  public void prePersist() {
-    this.createdAt = LocalDateTime.now();
-    this.updatedAt = LocalDateTime.now();
-  }
-
-  @PreUpdate
-  public void preUpdate() {
-    this.updatedAt = LocalDateTime.now();
-  }
 }
