12c087026ecc60cca0f1a5b01a909d641204f49a
sezeme <stellaa223@gmail.com>
2025-06-27 19:39:01 +0900

    feat: add controller

---
+++ src/main/java/com/nexus/sion/feature/member/command/application/controller/GradeCommandController.java
@@ -0,0 +1,31 @@
+package com.nexus.sion.feature.member.command.application.controller;
+
+import com.nexus.sion.common.dto.ApiResponse;
+import com.nexus.sion.feature.member.command.application.dto.request.MemberCreateRequest;
+import com.nexus.sion.feature.member.command.application.dto.request.UnitPriceSetRequest;
+import com.nexus.sion.feature.member.command.application.service.GradeCommandService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/grades")
+@Tag(name = "", description = "")
+public class GradeCommandController {
+
+    private final GradeCommandService gradeCommandService;
+
+    @Operation(summary = "구간별 단가 설정", description = "구간별 단가 설정 기능")
+    @PostMapping
+    public ResponseEntity<ApiResponse<Void>> setUnitPriceByGrade(@RequestBody UnitPriceSetRequest request) {
+        gradeCommandService.setUnitPriceByGrade(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(null));
+    }
+}
