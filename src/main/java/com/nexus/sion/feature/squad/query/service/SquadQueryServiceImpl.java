59c0fdc75b2c576a720e0f1a7d774152c8d71390
kihongkim <clevermind2@naver.com>
2025-06-27 20:15:16 +0900

    스쿼드 페이징 관련 service 코드 추가

--- src/main/java/com/nexus/sion/feature/squad/query/service/SquadQueryServiceImpl.java
+++ src/main/java/com/nexus/sion/feature/squad/query/service/SquadQueryServiceImpl.java
@@ -1,15 +1,13 @@
 package com.nexus.sion.feature.squad.query.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import com.nexus.sion.exception.BusinessException;
 import com.nexus.sion.exception.ErrorCode;
 import com.nexus.sion.feature.squad.query.dto.request.SquadListRequest;
 import com.nexus.sion.feature.squad.query.dto.response.SquadDetailResponse;
-import com.nexus.sion.feature.squad.query.dto.response.SquadListResponse;
 import com.nexus.sion.feature.squad.query.repository.SquadQueryRepository;
+import com.nexus.sion.feature.squad.query.dto.response.SquadListResultResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,14 +18,14 @@ public class SquadQueryServiceImpl implements SquadQueryService {
   private final SquadQueryRepository squadQueryRepository;
 
   @Override
-  public List<SquadListResponse> findSquads(SquadListRequest request) {
-    List<SquadListResponse> squads = squadQueryRepository.findSquads(request);
+  public SquadListResultResponse findSquads(SquadListRequest request) {
+    SquadListResultResponse result = squadQueryRepository.findSquads(request);
 
-    if (squads == null || squads.isEmpty()) {
+    if (result == null || result.getContent().isEmpty()) {
       throw new BusinessException(ErrorCode.PROJECT_SQUAD_NOT_FOUND);
     }
 
-    return squads;
+    return result;
   }
 
   @Override
