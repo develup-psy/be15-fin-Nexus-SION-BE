7f9aa74fd6ca0082f5154833b7544e2793eb85a1
sezeme <stellaa223@gmail.com>
2025-06-29 18:08:28 +0900

    feat: fix dto

--- src/main/java/com/nexus/sion/feature/member/command/application/dto/request/InitialScoreDto.java
+++ src/main/java/com/nexus/sion/feature/member/command/application/dto/request/InitialScoreDto.java
@@ -1,11 +1,19 @@
 package com.nexus.sion.feature.member.command.application.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class InitialScoreDto {
-    long id;
     @NotBlank
-    int years;
+    Integer minYears;
+    Integer maxYears;
     @NotBlank
-    int score;
+    Integer score;
 }
