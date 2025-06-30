4054a6e9f28cbad4f5e04e3dd01f9288dfca7d3e
develup_psy <develup_psy@github.com>
2025-06-30 11:56:43 +0900

    fix: merge 완료

--- src/main/java/com/nexus/sion/exception/ErrorCode.java
+++ src/main/java/com/nexus/sion/exception/ErrorCode.java
@@ -42,7 +42,6 @@ public enum ErrorCode {
   FIRST_MIN_YEARS_SHOULD_BE_1("20015", "첫 번째 구간의 minYears는 1이어야 합니다.", HttpStatus.BAD_REQUEST),
   LAST_MAX_YEARS_SHOULD_BE_NULL("20016", "마지막 구간의 maxYears는 null이어야 합니다.", HttpStatus.BAD_REQUEST),
   INTERVAL_YEARS_SHOULD_BE_CONTINUOUS("20017", "구간은 연속되어야 합니다.", HttpStatus.BAD_REQUEST),
-  INVALID_SORT_COLUMN("20015", "유효하지 않는 정렬 기준입니다", HttpStatus.BAD_REQUEST ),
 
   // project
   PROJECT_CODE_DUPLICATED("30001", "이미 존재하는 프로젝트 코드입니다.", HttpStatus.CONFLICT),
