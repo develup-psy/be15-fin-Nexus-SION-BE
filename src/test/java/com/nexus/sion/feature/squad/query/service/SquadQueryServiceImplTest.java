945f8d52724909a9d541c9c964cffaf760a65e16
develup_psy <develup_psy@github.com>
2025-07-02 19:04:19 +0900

    refactor: SquadServiceImpl 빈 의존성 변경에 따른 테스트 셋업 수정

--- src/test/java/com/nexus/sion/feature/squad/query/service/SquadQueryServiceImplTest.java
+++ src/test/java/com/nexus/sion/feature/squad/query/service/SquadQueryServiceImplTest.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
+import com.nexus.sion.feature.squad.query.util.CalculateSquad;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,11 +29,14 @@ class SquadQueryServiceImplTest {
   private SquadQueryRepository squadQueryRepository;
   private SquadQueryServiceImpl squadQueryService;
   private SquadQueryMapper squadQueryMapper;
+  private CalculateSquad calculateSquad;
 
   @BeforeEach
   void setUp() {
     squadQueryRepository = Mockito.mock(SquadQueryRepository.class);
-    squadQueryService = new SquadQueryServiceImpl(squadQueryRepository, squadQueryMapper);
+    squadQueryMapper = Mockito.mock(SquadQueryMapper.class);
+    calculateSquad = Mockito.mock(CalculateSquad.class);
+    squadQueryService = new SquadQueryServiceImpl(squadQueryRepository, squadQueryMapper, calculateSquad);
   }
 
   @Test
