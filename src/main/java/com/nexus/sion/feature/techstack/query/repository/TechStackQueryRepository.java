8d6c95e3f2fbe97fbdc4cf5df31757f5eb38d5d1
develup_psy <develup_psy@github.com>
2025-07-02 23:38:21 +0900

    feat: 기술스택 검색 자동완성 기능 구현

--- src/main/java/com/nexus/sion/feature/techstack/query/repository/TechStackQueryRepository.java
+++ src/main/java/com/nexus/sion/feature/techstack/query/repository/TechStackQueryRepository.java
@@ -6,6 +6,8 @@ import static com.nexus.sion.common.constants.CollationConstants.UTF8MB4_UNICODE
 import java.util.*;
 
 import org.jooq.DSLContext;
+import org.jooq.Field;
+import org.jooq.impl.DSL;
 import org.springframework.stereotype.Repository;
 
 import lombok.RequiredArgsConstructor;
@@ -21,5 +23,21 @@ public class TechStackQueryRepository {
         .from(TECH_STACK)
         .orderBy(TECH_STACK.TECH_STACK_NAME.collate(UTF8MB4_UNICODE_520_CI).asc())
         .fetchInto(String.class);
+  }
+
+  public List<String> findAutoCompleteTechStacks(String keyword) {
+    Field<String> nameField = TECH_STACK.TECH_STACK_NAME;
+
+    // CASE 구문으로 시작하는 경우 우선순위 부여
+    Field<Integer> orderPriority =
+            DSL.when(nameField.likeIgnoreCase(keyword + "%"), 0)
+                    .otherwise(1);
+
+    return dsl.select(nameField)
+            .from(TECH_STACK)
+            .where(nameField.containsIgnoreCase(keyword))
+            .orderBy(orderPriority.asc(), nameField.asc())
+            .limit(5)
+            .fetchInto(String.class);
   }
 }
