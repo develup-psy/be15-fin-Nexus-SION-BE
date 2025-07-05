632f5431bb25960e055b52fea666f187c092162b
kihongkim <clevermind2@naver.com>
2025-07-06 02:40:20 +0900

    스쿼드 댓글 조회 서비스 구현

---
+++ src/main/java/com/nexus/sion/feature/squad/query/service/SquadCommentQueryServiceImpl.java
@@ -0,0 +1,22 @@
+package com.nexus.sion.feature.squad.query.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.nexus.sion.feature.squad.query.dto.response.SquadCommentResponse;
+import com.nexus.sion.feature.squad.query.repository.SquadCommentQueryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SquadCommentQueryServiceImpl implements SquadCommentQueryService {
+
+    private final SquadCommentQueryRepository squadCommentQueryRepository;
+
+    @Override
+    public List<SquadCommentResponse> findCommentsBySquadCode(String squadCode) {
+        return squadCommentQueryRepository.findBySquadCode(squadCode);
+    }
+}
