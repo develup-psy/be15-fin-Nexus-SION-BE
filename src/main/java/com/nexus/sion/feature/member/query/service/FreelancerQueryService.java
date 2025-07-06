951e5da349db282a35a6249fe5acc46cf9b93606
kimjy8937 <kimjy3520@gmail.com>
2025-07-06 23:43:02 +0900

    feat: 프리렌서 상세 조회 service interface 생성

--- src/main/java/com/nexus/sion/feature/member/query/service/FreelancerQueryService.java
+++ src/main/java/com/nexus/sion/feature/member/query/service/FreelancerQueryService.java
@@ -1,8 +1,10 @@
 package com.nexus.sion.feature.member.query.service;
 
 import com.nexus.sion.common.dto.PageResponse;
+import com.nexus.sion.feature.member.query.dto.response.FreelancerDetailResponse;
 import com.nexus.sion.feature.member.query.dto.response.FreelancerListResponse;
 
 public interface FreelancerQueryService {
   PageResponse<FreelancerListResponse> getFreelancers(int page, int size);
+  FreelancerDetailResponse getFreelancerDetail(String freelancerId);
 }
