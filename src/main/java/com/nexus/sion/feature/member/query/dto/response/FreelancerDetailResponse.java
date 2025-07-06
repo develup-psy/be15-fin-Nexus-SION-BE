37ba470e4c2840301011719f1703210a100e6513
kimjy8937 <kimjy3520@gmail.com>
2025-07-06 23:42:34 +0900

    feat: 프리렌서 상세 조회 response dto 생성

---
+++ src/main/java/com/nexus/sion/feature/member/query/dto/response/FreelancerDetailResponse.java
@@ -0,0 +1,14 @@
+package com.nexus.sion.feature.member.query.dto.response;
+
+import java.time.LocalDate;
+
+public record FreelancerDetailResponse(
+        String freelancerId,
+        String name,
+        String phoneNumber,
+        String email,
+        Integer careerYears,
+        String resumeUrl,
+        String profileImageUrl,
+        LocalDate birthday
+) {}
