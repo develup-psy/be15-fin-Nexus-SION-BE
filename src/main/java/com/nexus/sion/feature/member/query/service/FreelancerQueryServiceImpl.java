ad9044c9b644461ff244f2562d042ab807fba98e
kimjy8937 <kimjy3520@gmail.com>
2025-07-06 23:43:09 +0900

    feat: 프리렌서 상세 조회 service 구현체 구현

--- src/main/java/com/nexus/sion/feature/member/query/service/FreelancerQueryServiceImpl.java
+++ src/main/java/com/nexus/sion/feature/member/query/service/FreelancerQueryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.nexus.sion.feature.member.query.service;
 
+import com.nexus.sion.feature.member.query.dto.response.FreelancerDetailResponse;
 import org.springframework.stereotype.Service;
 
 import com.nexus.sion.common.dto.PageResponse;
@@ -17,5 +18,10 @@ public class FreelancerQueryServiceImpl implements FreelancerQueryService {
   @Override
   public PageResponse<FreelancerListResponse> getFreelancers(int page, int size) {
     return freelancerQueryRepository.getFreelancerList(page, size);
+  }
+
+  @Override
+  public FreelancerDetailResponse getFreelancerDetail(String freelancerId) {
+    return freelancerQueryRepository.getFreelancerDetail(freelancerId);
   }
 }
