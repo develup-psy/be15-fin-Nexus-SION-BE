8e8ddabeb7f77a63ebf63f664085972a040660fc
kimjy8937 <kimjy3520@gmail.com>
2025-07-07 00:21:30 +0900

    feat: 프리랜서 등록 Service 구현

---
+++ src/main/java/com/nexus/sion/feature/member/command/application/service/FreelancerCommandService.java
@@ -0,0 +1,56 @@
+package com.nexus.sion.feature.member.command.application.service;
+
+
+import com.nexus.sion.feature.member.command.domain.aggregate.entity.Member;
+import com.nexus.sion.feature.member.command.domain.aggregate.enums.MemberRole;
+import com.nexus.sion.feature.member.command.domain.aggregate.enums.MemberStatus;
+import com.nexus.sion.feature.member.command.domain.repository.FreelancerRepository;
+import com.nexus.sion.feature.member.command.domain.repository.MemberRepository;
+import com.nexus.sion.feature.member.query.dto.response.FreelancerDetailResponse;
+import com.nexus.sion.feature.member.query.repository.FreelancerQueryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class FreelancerCommandService {
+
+    private final FreelancerQueryRepository freelancerQueryRepository;
+    private final MemberRepository memberRepository;
+    private final FreelancerRepository freelancerRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void registerFreelancerAsMember(String freelancerId) {
+        FreelancerDetailResponse freelancer = freelancerQueryRepository.getFreelancerDetail(freelancerId);
+
+
+        String rawPassword = (freelancer.birthday() != null)
+                ? String.format("%02d%02d%02d",
+                freelancer.birthday().getYear() % 100,
+                freelancer.birthday().getMonthValue(),
+                freelancer.birthday().getDayOfMonth())
+                : "000000";
+
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+
+        Member member = Member.builder()
+                .employeeIdentificationNumber(freelancer.freelancerId())
+                .employeeName(freelancer.name())
+                .password(encodedPassword)
+                .profileImageUrl(freelancer.profileImageUrl())
+                .phoneNumber(freelancer.phoneNumber())
+                .email(freelancer.email())
+                .birthday(freelancer.birthday())
+                .careerYears(freelancer.careerYears())
+                .joinedAt(LocalDate.now())
+                .role(MemberRole.OUTSIDER)
+                .status(MemberStatus.AVAILABLE)
+                .build();
+
+        memberRepository.save(member);
+        freelancerRepository.deleteById(freelancer.freelancerId());
+    }
+}
