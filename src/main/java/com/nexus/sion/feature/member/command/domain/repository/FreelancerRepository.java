760a02104344c31ae08df3301aeab83d7baa686d
kimjy8937 <kimjy3520@gmail.com>
2025-07-07 00:21:42 +0900

    feat: 프리랜서 등록 Repository 구현

---
+++ src/main/java/com/nexus/sion/feature/member/command/domain/repository/FreelancerRepository.java
@@ -0,0 +1,8 @@
+package com.nexus.sion.feature.member.command.domain.repository;
+
+import com.nexus.sion.feature.member.command.domain.aggregate.entity.Freelancer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FreelancerRepository extends JpaRepository<Freelancer, String> {
+    // freelancer_id가 String(VARCHAR(30)) 이므로 ID 타입은 String
+}
