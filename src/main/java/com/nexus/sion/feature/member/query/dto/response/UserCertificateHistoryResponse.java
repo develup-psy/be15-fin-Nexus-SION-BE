24ee42dc3ddc1014d4912917e2c90422f62a0ba0
kihongkim <clevermind2@naver.com>
2025-07-08 16:38:42 +0900

    localdatetime -> localdate로 타입 변경

--- src/main/java/com/nexus/sion/feature/member/query/dto/response/UserCertificateHistoryResponse.java
+++ src/main/java/com/nexus/sion/feature/member/query/dto/response/UserCertificateHistoryResponse.java
@@ -1,5 +1,6 @@
 package com.nexus.sion.feature.member.query.dto.response;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import lombok.*;
@@ -13,7 +14,7 @@ public class UserCertificateHistoryResponse {
   private Long userCertificateHistoryId;
   private String certificateName;
   private String issuingOrganization;
-  private LocalDateTime issueDate;
+  private LocalDate issueDate;
   private String pdfFileUrl;
   private String certificateStatus;
   private String rejectedReason;
