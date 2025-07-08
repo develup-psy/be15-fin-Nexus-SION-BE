1b7101304cb528895f6d9f92273218f2363d9448
kihongkim <clevermind2@naver.com>
2025-07-08 16:14:18 +0900

    개발자 자격증(Certificate) 등록 Controller 클래스 생성

---
+++ src/main/java/com/nexus/sion/feature/member/command/application/controller/UserCertificateHistoryController.java
@@ -0,0 +1,28 @@
+package com.nexus.sion.feature.member.command.application.controller;
+
+import com.nexus.sion.common.dto.ApiResponse;
+import com.nexus.sion.feature.member.command.application.dto.request.UserCertificateHistoryRequest;
+import com.nexus.sion.feature.member.command.application.service.UserCertificateHistoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/members/{employeeId}/certificates")
+@RequiredArgsConstructor
+public class UserCertificateHistoryController {
+
+    private final UserCertificateHistoryService userCertificateHistoryService;
+
+    /* 개발자의 자격증 등록 */
+    @PostMapping
+    public ResponseEntity<ApiResponse<Void>> registerUserCertificate(
+            @PathVariable String employeeId,
+            @RequestBody @Valid UserCertificateHistoryRequest request
+    ) {
+        userCertificateHistoryService.registerUserCertificate(employeeId, request);
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+}
