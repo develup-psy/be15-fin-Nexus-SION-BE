b2439cd20ad1cd44fad51ed4a8e807d37fc3d6b8
kihongkim <clevermind2@naver.com>
2025-07-08 16:14:07 +0900

    개발자 자격증(Certificate) 등록 Entity 클래스 생성

---
+++ src/main/java/com/nexus/sion/feature/member/command/domain/aggregate/entity/UserCertificateHistory.java
@@ -0,0 +1,47 @@
+package com.nexus.sion.feature.member.command.domain.aggregate.entity;
+
+import com.nexus.sion.feature.member.command.domain.aggregate.enums.CertificateStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_certificate_history")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class UserCertificateHistory {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_certificate_history_id")
+  private Long id;
+
+  @Column(name = "issue_date", nullable = false)
+  private LocalDate issueDate;
+
+  @Column(name = "pdf_file_url", length = 255, nullable = false)
+  private String pdfFileUrl;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "certificate_status", nullable = false)
+  private CertificateStatus certificateStatus;
+
+  @Column(name = "rejected_reason", columnDefinition = "TEXT")
+  private String rejectedReason;
+
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt = LocalDateTime.now();
+
+  @Column(name = "updated_at", nullable = false)
+  private LocalDateTime updatedAt = LocalDateTime.now();
+
+  @Column(name = "employee_identification_number", length = 30, nullable = false)
+  private String employeeIdentificationNumber;
+
+  @Column(name = "certificate_name", length = 30, nullable = false)
+  private String  certificate;
+}
