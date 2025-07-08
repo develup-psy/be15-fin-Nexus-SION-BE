1de8838ff12066d0451306f3c249af37a60e81de
kihongkim <clevermind2@naver.com>
2025-07-08 16:13:39 +0900

    관리자 자격증(Certificate) 등록 Request DTO 클래스 생성

---
+++ src/main/java/com/nexus/sion/feature/member/command/application/dto/request/CertificateRequest.java
@@ -0,0 +1,17 @@
+package com.nexus.sion.feature.member.command.application.dto.request;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CertificateRequest {
+
+    @NotBlank(message = "자격증명은 필수입니다.")
+    private String certificateName;
+
+    @NotNull(message = "점수는 필수 항목입니다.")
+    private int score;
+}
