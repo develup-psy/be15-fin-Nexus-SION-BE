44287d2cad4ea6e5f4357139d7172dd31398c200
kihongkim <clevermind2@naver.com>
2025-07-08 16:14:58 +0900

    개발자 자격증(Certificate) 등록 Service 구현체 클래스 생성

---
+++ src/main/java/com/nexus/sion/feature/member/command/application/service/UserCertificateHistoryServiceImpl.java
@@ -0,0 +1,39 @@
+package com.nexus.sion.feature.member.command.application.service;
+
+import com.nexus.sion.feature.member.command.application.dto.request.UserCertificateHistoryRequest;
+import com.nexus.sion.feature.member.command.domain.aggregate.entity.Certificate;
+import com.nexus.sion.feature.member.command.domain.aggregate.entity.UserCertificateHistory;
+import com.nexus.sion.feature.member.command.domain.repository.CertificateRepository;
+import com.nexus.sion.feature.member.command.domain.repository.UserCertificateHistoryRepository;
+import com.nexus.sion.feature.member.command.domain.aggregate.enums.CertificateStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class UserCertificateHistoryServiceImpl implements UserCertificateHistoryService {
+
+    private final UserCertificateHistoryRepository userCertificateHistoryRepository;
+    private final CertificateRepository certificateRepository;
+
+    @Override
+    public void registerUserCertificate(String employeeId, UserCertificateHistoryRequest request) {
+        // 자격증명으로 certificate 조회
+        Certificate certificate = certificateRepository.findById(request.getCertificateName())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 자격증입니다."));
+
+        UserCertificateHistory history = UserCertificateHistory.builder()
+                .certificate(certificate.getCertificateName())
+                .employeeIdentificationNumber(employeeId)
+                .certificateStatus(CertificateStatus.PENDING)
+                .pdfFileUrl(request.getPdfFileUrl())
+                .issueDate(request.getIssueDate())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        userCertificateHistoryRepository.save(history);
+    }
+}
