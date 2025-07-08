f7bfd6c51997c17d7d6fa33cb3be5ddf6ba31fad
kihongkim <clevermind2@naver.com>
2025-07-08 15:19:24 +0900

    관리자 자격증(Certificate) Service 클래스 생성

---
+++ src/main/java/com/nexus/sion/feature/member/command/application/service/CertificateCommandService.java
@@ -0,0 +1,7 @@
+package com.nexus.sion.feature.member.command.application.service;
+
+import com.nexus.sion.feature.member.command.application.dto.request.CertificateRequest;
+
+public interface CertificateCommandService {
+    void registerCertificate(CertificateRequest request);
+}
