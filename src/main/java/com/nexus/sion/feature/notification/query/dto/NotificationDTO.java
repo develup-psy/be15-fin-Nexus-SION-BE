0dd8ca765413bb906f3a8e8b9279c2999dbdd771
sezeme <stellaa223@gmail.com>
2025-07-09 15:39:54 +0900

    feat: add dto

---
+++ src/main/java/com/nexus/sion/feature/notification/query/dto/NotificationDTO.java
@@ -0,0 +1,23 @@
+package com.nexus.sion.feature.notification.query.dto;
+
+import com.nexus.sion.feature.notification.command.domain.aggregate.NotificationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class NotificationDTO {
+    Long notificationId;
+    NotificationType notificationType;
+    String linkedContentId;
+    String message;
+    Boolean isRead;
+    LocalDateTime createdAt;
+    String senderId;
+    String senderName;
+    String receiverId;
+}
