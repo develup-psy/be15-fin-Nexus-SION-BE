33b2d012fa9606f38cedccd6023e82e575db8c54
kimjy8937 <kimjy3520@gmail.com>
2025-07-10 16:33:02 +0900

    feat: 등급 변경 test controller 구현

---
+++ src/main/java/com/nexus/sion/feature/member/command/application/controller/MemberGradeTestController.java
@@ -0,0 +1,20 @@
+package com.nexus.sion.feature.member.command.application.controller;
+
+import com.nexus.sion.feature.member.command.application.scheduler.MemberGradeScheduler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/test/grade")
+@RequiredArgsConstructor
+public class MemberGradeTestController {
+
+    private final MemberGradeScheduler memberGradeScheduler;
+
+    @PostMapping("/update")
+    public ResponseEntity<String> updateGradesManually() {
+        memberGradeScheduler.updateDeveloperGrades();
+        return ResponseEntity.ok("개발자 등급 산정 완료");
+    }
+}
