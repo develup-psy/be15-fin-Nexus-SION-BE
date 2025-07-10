7558c345dc5a9f8bf0c66b9e93cea9c7387b8bef
kihongkim <clevermind2@naver.com>
2025-07-10 16:19:13 +0900

    관리자 자격증 반려 request 코드 작성

---
+++ src/main/java/com/nexus/sion/feature/member/command/application/dto/request/CertificateRejectRequest.java
@@ -0,0 +1,12 @@
+package com.nexus.sion.feature.member.command.application.dto.request;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CertificateRejectRequest {
+    private String rejectedReason;
+}
+
