71c1f07b1f2ddd6ed79a1f5087ea1cd7a764ee42
kihongkim <clevermind2@naver.com>
2025-07-10 16:39:57 +0900

    setter 삭제

--- src/main/java/com/nexus/sion/feature/member/command/application/dto/request/CertificateRejectRequest.java
+++ src/main/java/com/nexus/sion/feature/member/command/application/dto/request/CertificateRejectRequest.java
@@ -1,10 +1,12 @@
 package com.nexus.sion.feature.member.command.application.dto.request;
 
 import lombok.Getter;
-import lombok.Setter;
 
-@Setter
 @Getter
 public class CertificateRejectRequest {
   private String rejectedReason;
+
+  public CertificateRejectRequest(String reason) {
+    this.rejectedReason = reason;
+  }
 }
