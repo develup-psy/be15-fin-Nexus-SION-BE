408e6c6d99940b5d823aee539d3af105163e5d4a
kihongkim <clevermind2@naver.com>
2025-07-11 18:18:45 +0900

    @RequestBody -> @ModelAttribute 변경

--- src/main/java/com/nexus/sion/feature/member/command/application/controller/UserCertificateHistoryController.java
+++ src/main/java/com/nexus/sion/feature/member/command/application/controller/UserCertificateHistoryController.java
@@ -22,7 +22,7 @@ public class UserCertificateHistoryController {
   // 개발자 자격증 등록
   @PostMapping
   public ResponseEntity<ApiResponse<Void>> registerUserCertificate(
-      @PathVariable String employeeId, @RequestBody @Valid UserCertificateHistoryRequest request) {
+      @PathVariable String employeeId, @ModelAttribute @Valid UserCertificateHistoryRequest request) {
     userCertificateHistoryService.registerUserCertificate(employeeId, request);
     return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(null));
   }
