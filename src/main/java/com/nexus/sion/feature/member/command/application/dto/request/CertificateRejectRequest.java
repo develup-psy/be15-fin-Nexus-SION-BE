95ccaaad74c1b3b56bdef94e57e5e5a148afd224
kihongkim <clevermind2@naver.com>
2025-07-11 18:14:12 +0900

    @NoArgsConstructor, @Setter 어노테이션 추가

--- src/main/java/com/nexus/sion/feature/member/command/application/dto/request/CertificateRejectRequest.java
+++ src/main/java/com/nexus/sion/feature/member/command/application/dto/request/CertificateRejectRequest.java
@@ -1,12 +1,12 @@
 package com.nexus.sion.feature.member.command.application.dto.request;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class CertificateRejectRequest {
   private String rejectedReason;
-
-  public CertificateRejectRequest(String reason) {
-    this.rejectedReason = reason;
-  }
 }
