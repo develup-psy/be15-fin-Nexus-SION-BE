35eeafb409851de938e2418615e22e67cb80f3e3
develup_psy <develup_psy@github.com>
2025-07-11 17:07:50 +0900

    chore: 기타 수정

--- src/main/java/com/nexus/sion/feature/techstack/command/domain/aggregate/TechStack.java
+++ src/main/java/com/nexus/sion/feature/techstack/command/domain/aggregate/TechStack.java
@@ -4,16 +4,15 @@ import jakarta.persistence.*;
 
 import com.nexus.sion.common.domain.BaseTimeEntity;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @Entity
 @Table(name = "tech_stack")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @ToString
+@Builder
+@AllArgsConstructor
 public class TechStack extends BaseTimeEntity {
   // base entity : 생성일자, 수정일자 자동생성 및 업데이트 설정
   @Id
