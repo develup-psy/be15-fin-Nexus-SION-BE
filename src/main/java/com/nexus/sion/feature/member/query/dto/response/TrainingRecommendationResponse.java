1a343d4ccf5629787b6d6eee3d705a767d6fd69a
kihongkim <clevermind2@naver.com>
2025-07-13 15:03:18 +0900

    교육추천 response 생성

---
+++ src/main/java/com/nexus/sion/feature/member/query/dto/response/TrainingRecommendationResponse.java
@@ -0,0 +1,45 @@
+package com.nexus.sion.feature.member.query.dto.response;
+
+import com.nexus.sion.feature.member.command.domain.aggregate.entity.TrainingProgram;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TrainingRecommendationResponse {
+
+  private Long trainingId;
+  private String trainingName;
+  private String trainingDescription;
+  private String imageUrl;
+  private String recommendationReason;
+  private String category;
+  private String organization;
+  private String videoUrl;
+
+  public static TrainingRecommendationResponse from(TrainingProgram p, String reason) {
+    return TrainingRecommendationResponse.builder()
+        .trainingId(p.getTrainingId())
+        .trainingName(p.getTrainingName())
+        .trainingDescription(p.getTrainingDescription())
+        .imageUrl(p.getImageUrl())
+        .recommendationReason(reason)
+        .category(p.getTrainingCategory())
+        .videoUrl(p.getVideoUrl())
+        .build();
+  }
+
+  public static TrainingRecommendationResponse from(TrainingProgramResponse p, String reason) {
+    return TrainingRecommendationResponse.builder()
+        .trainingId(p.getTrainingId())
+        .trainingName(p.getTrainingName())
+        .trainingDescription(p.getTrainingDescription())
+        .imageUrl(p.getImageUrl())
+        .recommendationReason(reason)
+        .category(p.getTrainingCategory())
+        .videoUrl(p.getVideoUrl())
+        .build();
+  }
+}
