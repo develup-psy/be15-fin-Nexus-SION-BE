38af38a8cbd784b990e68387bc3d693f5ebe0c44
kihongkim <clevermind2@naver.com>
2025-07-13 22:33:32 +0900

    스쿼드 등록, 수정, 삭제 단위 테스트 코드 작성

--- src/test/java/com/nexus/sion/feature/squad/command/application/service/SquadCommandServiceImplTest.java
+++ src/test/java/com/nexus/sion/feature/squad/command/application/service/SquadCommandServiceImplTest.java
@@ -2,38 +2,37 @@ package com.nexus.sion.feature.squad.command.application.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
+
+import com.nexus.sion.exception.BusinessException;
+import com.nexus.sion.exception.ErrorCode;
+import com.nexus.sion.feature.project.command.domain.aggregate.Project;
+import com.nexus.sion.feature.squad.command.application.dto.request.Developer;
+import com.nexus.sion.feature.squad.command.application.dto.request.SquadRegisterRequest;
+import com.nexus.sion.feature.squad.command.application.dto.request.SquadUpdateRequest;
+import com.nexus.sion.feature.squad.command.domain.aggregate.entity.Squad;
+import com.nexus.sion.feature.squad.command.domain.service.*;
+import com.nexus.sion.feature.squad.command.repository.SquadCommandRepository;
+import com.nexus.sion.feature.squad.command.repository.SquadCommentRepository;
+import com.nexus.sion.feature.squad.command.repository.SquadEmployeeCommandRepository;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
 
-import com.nexus.sion.exception.BusinessException;
-import com.nexus.sion.exception.ErrorCode;
-import com.nexus.sion.feature.project.command.domain.aggregate.Project;
-import com.nexus.sion.feature.project.command.domain.repository.ProjectRepository;
-import com.nexus.sion.feature.squad.command.application.dto.request.Developer;
-import com.nexus.sion.feature.squad.command.application.dto.request.SquadRegisterRequest;
-import com.nexus.sion.feature.squad.command.application.dto.request.SquadUpdateRequest;
-import com.nexus.sion.feature.squad.command.domain.aggregate.entity.Squad;
-import com.nexus.sion.feature.squad.command.domain.aggregate.entity.SquadEmployee;
-import com.nexus.sion.feature.squad.command.repository.SquadCommandRepository;
-import com.nexus.sion.feature.squad.command.repository.SquadCommentRepository;
-import com.nexus.sion.feature.squad.command.repository.SquadEmployeeCommandRepository;
-
 class SquadCommandServiceImplTest {
 
   @InjectMocks private SquadCommandServiceImpl squadCommandService;
+
   @Mock private SquadCommandRepository squadCommandRepository;
   @Mock private SquadEmployeeCommandRepository squadEmployeeCommandRepository;
-  @Mock private ProjectRepository projectRepository;
   @Mock private SquadCommentRepository squadCommentRepository;
+  @Mock private SquadValidationService squadValidationService;
 
   @BeforeEach
   void setUp() {
@@ -46,127 +45,134 @@ class SquadCommandServiceImplTest {
     // given
     Developer developer = Developer.builder().employeeId("EMP001").projectAndJobId(101L).build();
 
-    SquadRegisterRequest request =
-        SquadRegisterRequest.builder()
+    SquadRegisterRequest request = SquadRegisterRequest.builder()
             .projectCode("ha_1_1")
             .title("프론트엔드팀")
             .description("설명입니다.")
             .developers(List.of(developer))
+            .estimatedCost(BigDecimal.valueOf(5000000))
+
+            .estimatedDuration(BigDecimal.valueOf(3))
             .build();
 
     Project project = Project.builder().projectCode("ha_1_1").clientCode("ha_1").build();
 
-    when(projectRepository.findByProjectCode("ha_1_1")).thenReturn(Optional.of(project));
-
-    when(squadCommandRepository.countByProjectCode("ha_1_1")).thenReturn(0L);
+    given(squadValidationService.validateAndGetProject("ha_1_1")).willReturn(project);
+    willDoNothing().given(squadValidationService).validateSquadTitleUniqueForCreate("프론트엔드팀", "ha_1_1");
+    willDoNothing().given(squadValidationService).validateDevelopersExist(List.of(developer));
+    willDoNothing().given(squadValidationService).validateJobRequirements("ha_1_1", List.of(developer));
+    willDoNothing().given(squadValidationService).validateBudget(project, BigDecimal.valueOf(5000000));
+    willDoNothing().given(squadValidationService).validateDuration(project, BigDecimal.valueOf(3));
+    given(squadCommandRepository.countByProjectCode("ha_1_1")).willReturn(0L);
 
     // when
     squadCommandService.registerManualSquad(request);
 
     // then
-    verify(projectRepository).findByProjectCode("ha_1_1");
-    verify(squadCommandRepository).save(any(Squad.class));
-    ArgumentCaptor<List<SquadEmployee>> listCaptor = ArgumentCaptor.forClass(List.class);
-    verify(squadEmployeeCommandRepository).saveAll(listCaptor.capture());
-
-    List<SquadEmployee> capturedList = listCaptor.getValue();
-    assertThat(capturedList).hasSize(1);
-    assertThat(capturedList.get(0).getEmployeeIdentificationNumber()).isEqualTo("EMP001");
+    then(squadCommandRepository).should(times(1)).save(any(Squad.class));
+    then(squadEmployeeCommandRepository).should(times(1)).saveAll(anyList());
   }
 
   @Test
-  @DisplayName("스쿼드 등록 실패 - 프로젝트가 존재하지 않을 경우")
+  @DisplayName("스쿼드 등록 실패 - 프로젝트가 존재하지 않음")
   void registerManualSquad_projectNotFound() {
     // given
-    SquadRegisterRequest request =
-        SquadRegisterRequest.builder()
+    SquadRegisterRequest request = SquadRegisterRequest.builder()
             .projectCode("invalid_code")
             .title("스쿼드명")
             .description("설명")
             .developers(List.of())
             .build();
 
-    when(projectRepository.findByProjectCode("invalid_code")).thenReturn(Optional.empty());
+    given(squadValidationService.validateAndGetProject("invalid_code"))
+            .willThrow(new BusinessException(ErrorCode.PROJECT_NOT_FOUND));
 
     // when & then
     assertThatThrownBy(() -> squadCommandService.registerManualSquad(request))
-        .isInstanceOf(BusinessException.class)
-        .hasMessage(ErrorCode.PROJECT_NOT_FOUND.getMessage());
+            .isInstanceOf(BusinessException.class)
+            .hasMessage(ErrorCode.PROJECT_NOT_FOUND.getMessage());
 
-    verify(projectRepository).findByProjectCode("invalid_code");
-    verifyNoInteractions(squadCommandRepository);
-    verifyNoInteractions(squadEmployeeCommandRepository);
+    then(squadCommandRepository).shouldHaveNoInteractions();
+    then(squadEmployeeCommandRepository).shouldHaveNoInteractions();
   }
 
   @Test
-  @DisplayName("스쿼드 수정에 성공한다")
+  @DisplayName("스쿼드 수정 성공")
   void updateManualSquad_success() {
     // given
     String squadCode = "ha_1_1_1";
-    SquadUpdateRequest request =
-        SquadUpdateRequest.builder()
+    Developer dev1 = new Developer("EMP001", 101L, false);
+    Developer dev2 = new Developer("EMP002", 102L, false);
+
+    SquadUpdateRequest request = SquadUpdateRequest.builder()
             .squadCode(squadCode)
-            .title("수정된 스쿼드 제목")
-            .description("수정된 설명")
-            .developers(
-                List.of(new Developer("EMP001", 101L, false), new Developer("EMP002", 102L, false)))
+            .title("수정된 제목")
+            .description("수정 설명")
+            .developers(List.of(dev1, dev2))
+            .estimatedCost(BigDecimal.valueOf(4000000))
+            .estimatedDuration(BigDecimal.valueOf(2))
             .build();
 
-    Squad squad =
-        Squad.builder()
+    Squad squad = Squad.builder()
             .squadCode(squadCode)
             .projectCode("ha_1_1")
             .title("기존 제목")
             .description("기존 설명")
             .build();
 
+    Project project = Project.builder().projectCode("ha_1_1").build();
+
     given(squadCommandRepository.findBySquadCode(squadCode)).willReturn(Optional.of(squad));
+    given(squadValidationService.validateAndGetProject("ha_1_1")).willReturn(project);
+
+    willDoNothing().given(squadValidationService)
+            .validateSquadTitleUniqueForUpdate("수정된 제목", "ha_1_1", squadCode);
+    willDoNothing().given(squadValidationService).validateDevelopersExist(any());
+    willDoNothing().given(squadValidationService).validateJobRequirements(any(), any());
+    willDoNothing().given(squadValidationService).validateBudget(project, BigDecimal.valueOf(4000000));
+    willDoNothing().given(squadValidationService).validateDuration(project, BigDecimal.valueOf(2));
 
     // when
     squadCommandService.updateManualSquad(request);
 
     // then
-    assertThat(squad.getTitle()).isEqualTo("수정된 스쿼드 제목");
-    assertThat(squad.getDescription()).isEqualTo("수정된 설명");
+    assertThat(squad.getTitle()).isEqualTo("수정된 제목");
+    assertThat(squad.getDescription()).isEqualTo("수정 설명");
 
     then(squadEmployeeCommandRepository).should(times(1)).deleteBySquadCode(squadCode);
-
     then(squadEmployeeCommandRepository).should(times(1)).saveAll(anyList());
   }
 
   @Test
-  @DisplayName("존재하지 않는 스쿼드 코드로 수정 시 예외 발생")
-  void updateManualSquad_squadNotFound() {
+  @DisplayName("스쿼드 수정 실패 - 존재하지 않는 스쿼드")
+  void updateManualSquad_notFound() {
     // given
-    String squadCode = "invalid_code";
-
-    SquadUpdateRequest request =
-        SquadUpdateRequest.builder()
-            .squadCode(squadCode)
+    SquadUpdateRequest request = SquadUpdateRequest.builder()
+            .squadCode("invalid_code")
             .title("제목")
             .description("설명")
             .developers(List.of())
             .build();
 
-    given(squadCommandRepository.findBySquadCode(squadCode)).willReturn(Optional.empty());
+    given(squadCommandRepository.findBySquadCode("invalid_code")).willReturn(Optional.empty());
 
     // when & then
     assertThatThrownBy(() -> squadCommandService.updateManualSquad(request))
-        .isInstanceOf(BusinessException.class)
-        .hasMessageContaining("스쿼드");
+            .isInstanceOf(BusinessException.class)
+            .hasMessageContaining("스쿼드");
+
+    then(squadEmployeeCommandRepository).shouldHaveNoInteractions();
   }
 
   @Test
-  @DisplayName("스쿼드 삭제에 성공한다")
+  @DisplayName("스쿼드 삭제 성공")
   void deleteSquad_success() {
     // given
     String squadCode = "ha_1_1_1";
-    Squad squad =
-        Squad.builder()
+    Squad squad = Squad.builder()
             .squadCode(squadCode)
             .projectCode("ha_1_1")
             .title("삭제할 스쿼드")
-            .description("테스트용")
             .build();
 
     given(squadCommandRepository.findBySquadCode(squadCode)).willReturn(Optional.of(squad));
@@ -181,7 +187,7 @@ class SquadCommandServiceImplTest {
   }
 
   @Test
-  @DisplayName("스쿼드 삭제 실패 - 존재하지 않는 스쿼드 코드")
+  @DisplayName("스쿼드 삭제 실패 - 존재하지 않음")
   void deleteSquad_notFound() {
     // given
     String squadCode = "INVALID_CODE";
@@ -189,8 +195,8 @@ class SquadCommandServiceImplTest {
 
     // when & then
     assertThatThrownBy(() -> squadCommandService.deleteSquad(squadCode))
-        .isInstanceOf(BusinessException.class)
-        .hasMessage(ErrorCode.SQUAD_NOT_FOUND.getMessage());
+            .isInstanceOf(BusinessException.class)
+            .hasMessage(ErrorCode.SQUAD_NOT_FOUND.getMessage());
 
     then(squadEmployeeCommandRepository).shouldHaveNoInteractions();
     then(squadCommentRepository).shouldHaveNoInteractions();
